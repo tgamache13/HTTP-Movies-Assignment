17dbd3119fae57047be90e0dcbd37e7b14ff6cb9
Lambda_School_Loaner_260 <lambda_school_loaner_260@Tara-Mac.local>
2020-01-24 01:30:38 -0800

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import { Route } from "react-router-dom";
+
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import MovieForm from './Movies/MovieForm';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [updateMovie, setUpdateMovie] = useState();
+
+  useEffect(() => {
+    console.log('effect in app', updateMovie)
+  },[updateMovie])
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
@@ -18,7 +25,13 @@ const App = () => {
       <Route
         path="/movies/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+          return <Movie {...props} addToSavedList={addToSavedList} setUpdateMovie={setUpdateMovie}/>;
+        }}
+      />
+      <Route
+        path='/update-movie/:id'
+        render={props => {
+          return <MovieForm {...props} changeMovie={setUpdateMovie} updateMovie={updateMovie}/>
         }}
       />
     </>
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
+
 export default class Movie extends React.Component {
   constructor(props) {
     super(props);
@@ -31,6 +32,21 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  updateMovie = ()=> {
+    this.props.setUpdateMovie(this.state.movie);
+    this.props.history.push(`/update-movie/${this.props.match.params.id}`);
+  }
+
+  deleteMovie = () => {
+    axios
+      .delete(`http://localhost:5000/api/movies/${this.state.movie.id}`, this.state.movie)
+      .then(res => {
+        console.log('delete movie', res);
+        this.props.history.push('/');
+      })
+      .catch(err => console.log('error during delete', err))
+  }
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -41,6 +57,12 @@ export default class Movie extends React.Component {
         <MovieCard movie={this.state.movie} />
         <div className="save-button" onClick={this.saveMovie}>
           Save
+        </div>
+        <div className="update-button" onClick={this.updateMovie}>
+          Update
+        </div>
+        <div className='delete-button'onClick={this.deleteMovie}>
+          Delete
         </div>
       </div>
     );
